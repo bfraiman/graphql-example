6be0e251afaf6f0044e4be812d4099dbb8b61f55
Brian <brian.fraiman@valtech.com>
2019-09-16 15:46:03 -0300

    Example of graphql api with custom types

--- app.js
+++ app.js
@@ -5,18 +5,35 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -25,8 +42,20 @@ app.use(
         }
     `),
     rootValue: {
-      events: () => ["Sailing", "Romantic Cooking"],
-      createEvent: args => args.name
+      events: () => events,
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+
+        events.push(event);
+
+        return event;
+      }
     },
     graphiql: true
   })
